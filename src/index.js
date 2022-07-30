dda77e30f4cfcbd6fa7065d522387d47b83cc973
poornesh h n <poorneshhn3@gmail.com>
2022-07-30 14:49:32 +0530

    solve cors error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const app = require("express")();
-const { videos } = require("../data/allvideos");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const data = require("../data/allvideos");
 
 app.get("/v1/videos", (req, res) => {
-  res.status(200).json(videos);
+  res.status(200).json(data);
 });
+
+app.post("/v1/videos", (req, res) => {
+  res.status(201).json({ status: "success" });
+});
+
 app.listen(process.env.PORT || 8000, () => {
   console.log("Port is running on ", process.env.PORT || 8000);
 });
